193e450d6ad263de0416213ed74b8bd5b3466677
@hexlet/cli <support@hexlet.io>
2022-12-11 21:55:50 +0300

    submit java-web-ru/deploy

--- java-web-ru/deploy/src/main/java/exercise/servlet/CompaniesServlet.java
+++ java-web-ru/deploy/src/main/java/exercise/servlet/CompaniesServlet.java
@@ -7,8 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import static exercise.Data.getCompanies;
 
 public class CompaniesServlet extends HttpServlet {
@@ -21,13 +23,20 @@ public class CompaniesServlet extends HttpServlet {
         // BEGIN
         String search = request.getParameter("search");
         List<String> companies = getCompanies();
+        List<String> companiesToPrint = new ArrayList<>();
         PrintWriter out = response.getWriter();
-        for (String e : companies) {
-            if (search != null && !search.equals("") && !search.equals("\"\"")) {
-                if (e.contains(search)) {
-                    out.println(e);
-                }
-            } else {
+        if (search != null && !search.equals("")) {
+            companiesToPrint = companies.stream()
+                    .filter(c -> c.contains(search))
+                    .collect(Collectors.toList());
+        } else {
+            companiesToPrint.addAll(companies);
+        }
+
+        if (companiesToPrint.isEmpty()) {
+            out.println("Companies not found");
+        } else {
+            for (String e : companiesToPrint) {
                 out.println(e);
             }
         }
