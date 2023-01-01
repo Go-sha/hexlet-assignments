f4e0c2b8ca4aa5048b6e2fa3cae39504c1d8ab27
@hexlet/cli <support@hexlet.io>
2023-01-01 19:17:41 +0300

    submit java-spring-ru/nested-resources

---
+++ java-spring-ru/nested-resources/src/main/java/exercise/repository/CommentRepository.java
@@ -0,0 +1,18 @@
+package exercise.repository;
+
+import exercise.model.Comment;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CommentRepository extends CrudRepository<Comment, Long> {
+
+    // BEGIN
+    Iterable<Comment> findAllByPostId(long postId);
+    Optional<Comment> findByIdAndPostId(Long id, Long postId);
+    // END
+}
